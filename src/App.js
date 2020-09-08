0d7c658946fe56642c63947c8cbe00f0552bc0c4
Vibhu Khurana <kkhurana.vibhu@gmail.com>
2020-09-08 22:31:53 +0530

    cart dropdown will hide on add to cart button click, checkout component added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
+import CheckoutPage from './pages/checkout/checkout.component';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -46,6 +47,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route
             path='/signIn'
             exact
