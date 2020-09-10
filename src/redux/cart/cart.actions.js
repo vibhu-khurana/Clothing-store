b3ea79e86056a92acca01db8e460719b9d6a2540
Vibhu Khurana <kkhurana.vibhu@gmail.com>
2020-09-10 13:47:14 +0530

    remove items from cart logic added

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,8 @@ export const addItem = (item) => ({
   type: CartActionTypes.ADD_TO_CART,
   payload: item,
 });
+
+export const clearItemFromCart = (item) => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
