0857456fea3dfdf5166765e7334bf208135176b2
Vibhu Khurana <kkhurana.vibhu@gmail.com>
2020-09-27 17:15:20 +0530

    redux devtool added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
@@ -10,7 +11,10 @@ if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
 
 export const persistor = persistStore(store);
 
